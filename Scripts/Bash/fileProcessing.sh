01ac2b23ef34056ab5998d42c3b4d64119d1d909
hufeng <zhouhufeng@gmail.com>
2022-06-08 11:52:04 -0400

    cut

--- Scripts/Bash/fileProcessing.sh
+++ Scripts/Bash/fileProcessing.sh
@@ -1,3 +1,3 @@
 head -3000000 hg19_CADD_SNVs_inclAnno_chr22.txt | sed 's/\t/,/g' > hg19_CADD_SNVs_inclAnno_chr22_h3M.csv
 head -3000 hg19_CADD_SNVs_inclAnno_chr22.txt > hg19_CADD_SNVs_inclAnno_chr22_h3k.tsv
-
+cut -f 1-4 hg19_CADD_SNVs_inclAnno_chr22.txt > hg19_CADD_SNVs_inclAnno_chr22_F1-4.txt
